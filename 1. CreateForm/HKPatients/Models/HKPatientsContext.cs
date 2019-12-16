2e8fd086c41c0139115c4ea9c49a89d12ceb1e45
Allison <heuijin.ko@gmail.com>
2019-12-15 21:00:24 -0500

    Edit readonly

--- 1. CreateForm/HKPatients/Models/HKPatientsContext.cs
+++ 1. CreateForm/HKPatients/Models/HKPatientsContext.cs
@@ -34,8 +34,7 @@ namespace HKPatients.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-85PVOMF\\SQLEXPRESS;Initial Catalog=Patients;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("HKPatientsConnection");
             }
         }
 
